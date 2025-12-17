aecce89ae23915641a590a16f84e8e840ef1c944
AayushNeupane1 <aayush.neupane023@apexcollege.edu.np>
2025-12-17 10:13:20 +0545

    mini assignment 1

---
+++ MiniAssignment/1/basic.py
@@ -0,0 +1,11 @@
+#print full name
+
+print("Aayush Neupane")
+
+#print fav quote
+
+print("Go with the flow but dont be a dead fish")
+
+#3. Print a sentence in a multiple line using 
+
+print("This is example of \n of breaking sentence")
